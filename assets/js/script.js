55c887aec76ef2ed5bd1de414dd6971c963ec72a
Chasster <110549152+ChassterTP@users.noreply.github.com>
2024-09-06 21:52:58 +0800

    update assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,6 @@ console.log('Script Loaded');
 function send() {
   fetch('https://ntfy.sh/ChassterTest', {
   method: 'POST', // PUT works too
-  body: 'Sent by your site'
+  body: 'Someone visited your site'
 })
 }
